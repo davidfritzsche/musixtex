15a084f43ba1afe4d81f408aeeb88639e68f7d20
David Fritzsche <david.fritzsche@mvua.de>
2015-10-23 14:15:23 +0200

    MusiXTeX 1.16

--- doc/musixtex/manualexa/BatchCommands/latexpdfall.bat
+++
@@ -1,3 +0,0 @@
-rem proces all files with extension pmx in this directory
-for /F %%I in ('dir /B *.tex') do call lapdfview.bat %%~nI
-pause
--- doc/musixtex/manualexa/BatchCommands/texall.bat
+++
@@ -1,4 +0,0 @@
-rem proces all files with extension pmx in this directory
-pause
-for /F %%I in ('dir /B *.tex') do call mupsview.bat %%~nI
-pause
