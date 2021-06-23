cea273a1d771f265fb8b75d7de3532e8c67f5280
David Fritzsche <dfritzsche@daffi.net>
2021-06-23 18:11:33 +0200

    MusiXTeX 1.30
      *  defined \zdoublebar for use within a bar
      *  defined \hbp and \hbpp

--- windows/musixtex.bat
+++ windows/musixtex.bat
@@ -1,5 +1,5 @@
 :: wrapper script to call musixtex.lua
-:: Usage: musixtex [options] basename[.tex]
+:: Usage: musixtex [options] basename[.tex|.ltx|.mtx|.pmx|.aspc]
 :: Suggested by Tomasz Luczak (Tomek) t34www@googlemail.com
 
 @for /f "delims=" %%I in ('kpsewhich --format=texmfscripts %~n0.lua') do texlua "%%I" %*
